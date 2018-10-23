ff0090045ada8a04bcfc02f9cac9cb376c164751
your name <your email>
2018-10-23 14:56:00 +0000

    Extra work: Integer tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,23 @@
+class Integer
+  def fizzbuzz
+    if self % 15 == 0
+      return "fizzbuzz"
+    elsif self % 5 == 0
+      return "buzz"
+    elsif self % 3 == 0
+      return "fizz"
+    else 
+      return self
+    end
+  end 
+end 
+
+def fizzbuzz_to_20
+  output = []
+  20.times { |i| output << (fizzbuzz(i+1))}
+  return output.join("\n")
+end
+
 def fizzbuzz(number)
   if number % 15 == 0
     return "fizzbuzz"
@@ -9,9 +29,3 @@ def fizzbuzz(number)
     return number
   end 
 end 
-
-def fizzbuzz_to_20
-  output = []
-  20.times { |i| output << (fizzbuzz(i+1))}
-  return output.join("\n")
-end
